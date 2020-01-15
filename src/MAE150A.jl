9a39c473725b17f7160860662eae01309d589d06
Jeff Eldredge <jdeldre@g.ucla.edu>
2020-01-14 16:32:49 -0800

    Added a load function

--- src/MAE150A.jl
+++ src/MAE150A.jl
@@ -1,7 +1,38 @@
 module MAE150A
 
-  function myfunc(x)
-    return 2*x
+  using Reexport
+
+  @reexport using ViscousFlow
+  @reexport using Plots
+  @reexport using OrdinaryDiffEq
+
+
+  function load_ns_solution(filen)
+
+    d = load(filen)
+
+    body = d["body"]
+    g = d["grid"]
+    Δt = d["Δt"]
+    U∞ = d["U∞"]
+    Re = d["Re"]
+    w = d["u"]
+
+    X = VectorData(body.x,body.y);
+    sys = NavierStokes(Re,cellsize(g),limits(g,1),limits(g,2),Δt,
+                        U∞ = U∞, X̃ = X, isstore = true)
+
+    xg, yg = coordinates(w,sys.grid)
+    ω = vorticity(w,sys)
+    q = velocity(w,sys)
+
+    q.u .+= sys.U∞[1]
+    q.v .+= sys.U∞[2]
+    ψ = streamfunction(w,sys) #.+ sys.U∞[1]*yg'
+    ψ .+= sys.U∞[1]*yg';
+
+    return sys, body, q, ω, ψ                  
+
   end
 
 end
