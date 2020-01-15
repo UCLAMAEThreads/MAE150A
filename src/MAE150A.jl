0b29c3b41c710f9b6c0b1f88a70204baa8b80b9e
Jeff Eldredge <jdeldre@g.ucla.edu>
2020-01-15 15:05:14 -0800

    Added load and save solution routines

--- src/MAE150A.jl
+++ src/MAE150A.jl
@@ -7,8 +7,34 @@ module MAE150A
   @reexport using OrdinaryDiffEq
   using JLD
 
-  export load_ns_solution, compute_trajectory
+  export save_ns_solution,load_ns_solution, compute_trajectory
 
+  """
+      save_ns_solution(filen::String,state,f,sys,body)
+
+  Save a state vector `state` of the Navier-Stokes solution in a file with the provided
+  filenam `filen`. Also save the associated vector of Lagrange forces `f`, the
+  system solution metadata `sys`, and the body data `body`.
+  """
+  function save_ns_solution(filen,sys,body,state,f)
+
+    save(filen,"Re",sys.Re,"U∞",sys.U∞,"Δt",sys.Δt,"grid",sys.grid,"body",body,"state",state,"f",f)
+    return nothing
+
+  end
+
+  """
+      load_ns_solution(filen::String)
+
+  Given a file name `filen` in JLD format, load in the Navier-Stokes solution data
+  stored in this file and set up various solution variables. An example:
+  ```
+  q, ω, ψ, f, sys, body = load_ns_solution("myfile.jld")
+  ```
+  In this example, `q` is the velocity field, `ω` the vorticity field,
+  `ψ` the streamfunction, and `f` the vector of Lagrange forces on the body,
+  `sys` is the NS system metadata and operators, and `body` the body data.
+  """
   function load_ns_solution(filen)
 
     d = load(filen)
@@ -18,7 +44,8 @@ module MAE150A
     Δt = d["Δt"]
     U∞ = d["U∞"]
     Re = d["Re"]
-    w = d["u"]
+    w = d["state"]
+    f = d["f"]
 
     X = VectorData(body.x,body.y);
     sys = NavierStokes(Re,cellsize(g),limits(g,1),limits(g,2),Δt,
@@ -33,9 +60,11 @@ module MAE150A
     ψ = streamfunction(w,sys) #.+ sys.U∞[1]*yg'
     ψ .+= sys.U∞[1]*yg';
 
-    return sys, body, q, ω, ψ
+    return q, ω, ψ, f, sys, body
 
   end
+
+### TRAJECTORY CALCULATION ###
 
   function _vfcn!(dR,R,p,t,u,v)
     dR[1] = u(R[1],R[2])
