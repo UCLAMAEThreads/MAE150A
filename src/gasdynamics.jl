1a083ded3ad6319c1f3417e344efb8f2cb1e8930
Jeff Eldredge <jdeldre@g.ucla.edu>
2020-03-10 23:29:50 -0700

    Added Fanno flow

--- src/gasdynamics.jl
+++ src/gasdynamics.jl
@@ -104,6 +104,31 @@ default_unit(::Type{T}) where {T<:GasConstantUnits} = JPerKGK
 EntropyUnits = Union{JPerKGK}
 default_unit(::Type{T}) where {T<:EntropyUnits} = JPerKGK
 
+# mass flow rate
+abstract type KGPerSec <: SI end
+MassFlowRateUnits = Union{KGPerSec}
+default_unit(::Type{T}) where {T<:MassFlowRateUnits} = KGPerSec
+
+# length
+abstract type Meters <: SI end
+abstract type CM <: SIOtherUnits end
+LengthUnits = Union{Meters,CM}
+default_unit(::Type{LengthUnits}) = Meters
+convert_unit(::Type{Meters},::Type{CM},val) = val*1e-2
+convert_unit(::Type{CM},::Type{Meters},val) = val*1e2
+
+# diameter
+DiameterUnits = LengthUnits
+default_unit(::Type{T}) where {T<:DiameterUnits} = default_unit(LengthUnits)
+
+# friction factor f
+FrictionFactorUnits = Dimensionless
+default_unit(::Type{T}) where {T<:FrictionFactorUnits} = Dimensionless
+
+# f*L/D
+FLOverDUnits = Dimensionless
+default_unit(::Type{T}) where {T<:FLOverDUnits} = Dimensionless
+
 
 ###### THERMODYNAMIC PROCESSES #######
 
@@ -111,6 +136,9 @@ abstract type ThermodynamicProcess end
 
 abstract type Isentropic <: ThermodynamicProcess end
 abstract type NormalShock <: ThermodynamicProcess end
+abstract type FannoFlow <: ThermodynamicProcess end
+abstract type RayleighFlow <: ThermodynamicProcess end
+
 
 ###### THERMODYNAMIC QUANTITIES #######
 
@@ -239,7 +267,8 @@ for qty in ("Pressure","Temperature","Density","Enthalpy","InternalEnergy","Soun
 end
 
 # other thermodynamic state quantities
-for qty in ("Area","MachNumber","Entropy")
+for qty in ("Area","MachNumber","Entropy","MassFlowRate","Velocity",
+            "Length","Diameter","FrictionFactor","FLOverD")
 
     qtysym = Symbol(qty)
     unitsym = Symbol(qty,"Units")
@@ -253,7 +282,8 @@ for qty in ("Area","MachNumber","Entropy")
 end
 
 # ratios of thermodynamic quantities
-for qtybase in ("Area","Pressure","Temperature","Density","MachNumber","StagnationPressure")
+for qtybase in ("Area","Pressure","Temperature","Density","MachNumber",
+                "StagnationPressure","Velocity")
 
     qty = qtybase*"Ratio"
     qtysym = Symbol(qty)
@@ -273,6 +303,12 @@ for qtybase in ("Area","Pressure","Temperature","Density","MachNumber","Stagnati
 end
 
 #=
+Geometric relationships
+=#
+Area(D::Diameter) = Area(π*D^2/4)
+Diameter(A::Area) = Diameter(sqrt(4*A/π))
+
+#=
 Thermodynamic relationships
 =#
 
@@ -281,6 +317,11 @@ Thermodynamic relationships
 Temperature(ρ::Density,p::Pressure;gas::PerfectGas=DefaultPerfectGas) = Temperature(p/(ρ*GasConstant(gas)))
 Density(p::Pressure,T::Temperature;gas::PerfectGas=DefaultPerfectGas) = Density(p/(GasConstant(gas)*T))
 Pressure(ρ::Density,T::Temperature;gas::PerfectGas=DefaultPerfectGas) = Pressure(ρ*GasConstant(gas)*T)
+
+StagnationTemperature(ρ0::StagnationDensity,p0::StagnationPressure;gas::PerfectGas=DefaultPerfectGas) = StagnationTemperature(p0/(ρ0*GasConstant(gas)))
+StagnationDensity(p0::StagnationPressure,T0::StagnationTemperature;gas::PerfectGas=DefaultPerfectGas) = StagnationDensity(p0/(GasConstant(gas)*T0))
+StagnationPressure(ρ0::StagnationDensity,T0::StagnationTemperature;gas::PerfectGas=DefaultPerfectGas) = StagnationPressure(ρ0*GasConstant(gas)*T0)
+
 
 """
     SoundSpeed(T::Temperature[,γ::SpecificHeatRatio=1.4][,R::GasConstant=287])
@@ -292,11 +333,28 @@ can also be supplied, but default to the values for air at standard conditions
 SoundSpeed(T::Temperature;gas::PerfectGas=DefaultPerfectGas) = SoundSpeed(sqrt(SpecificHeatRatio(gas)*GasConstant(gas)*T))
 
 Enthalpy(T::Temperature;gas::PerfectGas=DefaultPerfectGas) = Enthalpy(SpecificHeatPressure(gas)*T)
+Temperature(h::Enthalpy;gas::PerfectGas=DefaultPerfectGas) = Temperature(h/SpecificHeatPressure(gas))
+
 StagnationEnthalpy(T0::StagnationTemperature;gas::PerfectGas=DefaultPerfectGas) = StagnationEnthalpy(SpecificHeatPressure(gas)*T0)
+StagnationTemperature(h0::StagnationEnthalpy;gas::PerfectGas=DefaultPerfectGas) = StagnationTemperature(h0/SpecificHeatPressure(gas))
 
 InternalEnergy(T::Temperature;gas::PerfectGas=DefaultPerfectGas) = InternalEnergy(SpecificHeatVolume(gas)*T)
-StagnationInternalEnergy(T0::StagnationTemperature;gas::PerfectGas=DefaultPerfectGas) = StagnationInternalEnergy(SpecificHeatVolume(gas)*T0)
+Temperature(e::InternalEnergy;gas::PerfectGas=DefaultPerfectGas) = Temperature(e/SpecificHeatVolume(gas))
 
+StagnationInternalEnergy(T0::StagnationTemperature;gas::PerfectGas=DefaultPerfectGas) = StagnationInternalEnergy(SpecificHeatVolume(gas)*T0)
+StagnationTemperature(e0::StagnationInternalEnergy;gas::PerfectGas=DefaultPerfectGas) = StagnationTemperature(e0/SpecificHeatVolume(gas))
+
+StagnationEnthalpy(h::Enthalpy,u::Velocity) = StagnationEnthalpy(value(h)+0.5*value(u)^2)
+Enthalpy(h0::StagnationEnthalpy,u::Velocity) = Enthalpy(value(h0)-0.5*value(u)^2)
+
+
+# Mach number - velocity relations
+Velocity(a::SoundSpeed,M::MachNumber) = Velocity(M*a)
+MachNumber(u::Velocity,a::SoundSpeed) = MachNumber(u/a)
+SoundSpeed(u::Velocity,M::MachNumber) = SoundSpeed(u/M)
+
+# mass flow rate
+MassFlowRate(ρ::Density,u::Velocity,A::Area) = MassFlowRate(ρ*u*A)
 
 function T0OverT(M::MachNumber;gas::PerfectGas=DefaultPerfectGas)
     γ = SpecificHeatRatio(gas)
@@ -436,6 +494,49 @@ function Entropy(s1::Entropy,M1::MachNumber,::Type{NormalShock};gas::PerfectGas=
     ds = cv*(log(1+2*γ/(γ+1)*(M1^2-1)) - γ*log((γ+1)*M1^2/(2+(γ-1)*M1^2)))
     return Entropy(s1+ds)
 
+end
+
+######## FANNO FLOW ########
+
+
+function FLStarOverD(M::MachNumber,::Type{FannoFlow};gas::PerfectGas=DefaultPerfectGas)
+  γ = SpecificHeatRatio(gas)
+  return FLOverD((1-M^2)/(γ*M^2) + (γ+1)/(2γ)*log((γ+1)*M^2/(2+(γ-1)*M^2)))
+end
+
+function MachNumber(fL_over_D::FLOverD;gas::PerfectGas=DefaultPerfectGas)
+    Msub = find_zero(x -> FLStarOverD(MachNumber(x),FannoFlow,gas=gas)-fL_over_D,(0.001,1),order=16)
+    max_fLD = FLStarOverD(MachNumber(1e10),FannoFlow,gas=gas)
+
+    if value(fL_over_D) > value(max_fLD)
+        return MachNumber(Msub)
+    else
+        Msup = find_zero(x -> FLStarOverD(MachNumber(x),FannoFlow,gas=gas)-fL_over_D,(1,1e10),order=16)
+        return MachNumber(Msub), MachNumber(Msup)
+    end
+end
+
+FLOverD(f::FrictionFactor,L::Length,D::Diameter) = FLOverD(f*L/D)
+Length(fLoverD::FLOverD,D::Diameter,f::FrictionFactor) = Length(fLoverD*D/f)
+
+function POverPStar(M::MachNumber,::Type{FannoFlow};gas::PerfectGas=DefaultPerfectGas)
+  γ = SpecificHeatRatio(gas)
+  return PressureRatio(1/M*sqrt((1+γ)/(2+(γ-1)*M^2)))
+end
+
+function ρOverρStar(M::MachNumber,::Type{FannoFlow};gas::PerfectGas=DefaultPerfectGas)
+  γ = SpecificHeatRatio(gas)
+  return DensityRatio(1/M*sqrt((2+(γ-1)*M^2)/(γ+1)))
+end
+
+function TOverTStar(M::MachNumber,::Type{FannoFlow};gas::PerfectGas=DefaultPerfectGas)
+  γ = SpecificHeatRatio(gas)
+  return TemperatureRatio((γ+1)/(2+(γ-1)*M^2))
+end
+
+function P0OverP0Star(M::MachNumber,::Type{FannoFlow};gas::PerfectGas=DefaultPerfectGas)
+  γ = SpecificHeatRatio(gas)
+  return StagnationPressureRatio(1/M*((2+(γ-1)*M^2)/(γ+1))^((γ+1)/(2(γ-1))))
 end
 
 nothing
