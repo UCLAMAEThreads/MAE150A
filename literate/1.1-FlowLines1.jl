7623c6c8c4cbdab212f121e28812052d73e0f73c
Jeff Eldredge <jdeldre@g.ucla.edu>
2021-01-13 17:09:42 -0800

    Added literate directory and a few literate source files

---
+++ literate/1.1-FlowLines1.jl
@@ -0,0 +1,217 @@
+# <img src="logo_UCLA_blue_boxed.svg" alt="ucla logo" width="150" align="left"/>
+
+# ### **MAE 150A - Intermediate Fluid Mechanics**
+# <hr />
+
+#=
+# Flow lines and the material derivative in a steady flow field
+In this notebook we will discuss:
+* Path lines (particle trajectories)
+* The material derivative
+* Streamlines and the streamfunction
+
+and observe them in a pre-computed steady flow field.
+=#
+
+# ### Set up the module
+using MAE150A
+#-
+using Plots
+
+#=
+### Load the flow from file
+First, let us load the flow from a pre-computed CFD solution. This is the **steady state**
+flow past a NACA 4415 airfoil at 5 degrees angle of attack at Reynolds number 500.
+=#
+
+#=
+In the command below, `u` contains the solution field, `t` the time (which is not that
+important in this problem, since it is steady), and `sys` contains some operators for quickly
+getting other information about the flow. It takes a few seconds for this to do its work,
+so be patient:
+=#
+filename = "NACA4415Re500.jld"
+u, t, sys = load_ns_solution(filename);
+
+#=
+For our immediate use, let's get the velocity field (`vel`) for this flow. We get that with
+the `velocity` function in the `ViscousFlow` package (which is loaded into the `MAE150A`
+package). This velocity field will serve a lot of our discussion below.
+=#
+vel = ViscousFlow.velocity(u,sys,t);
+
+#=
+## Particle trajectories (i.e., pathlines)
+First, let us discuss particle trajectories.
+=#
+
+#=
+A particle moves at a velocity $\mathbf{V}(t)$ equal to the fluid velocity field,
+$\mathbf{u}(\mathbf{x},t)$, evaluated at the particle's location $\mathbf{r}(t)$.
+We can write this mathematically as
+
+$$\mathbf{V}(t) = \mathbf{u}(\mathbf{r}(t),t)$$
+
+This means that the particle's trajectory, $\mathbf{r}(t)$, is determined by the equation
+
+$$ \dfrac{\mathrm{d}\mathbf{r}}{\mathrm{d}t} = \mathbf{u}(\mathbf{r}(t),t) $$
+
+If we integrate this equation over time, starting from some initial location $\mathbf{r}(0)$,
+then we get the trajectory of a single fluid particle.
+
+If the velocity field is **steady**, then this equation simplifies a bit, because we lose the
+second argument ($t$) of $\mathbf{u}$, and we thus have only one unchanging velocity field to
+integrate.
+
+If we want to look at trajectories of many particles, then we simply integrate this equation for each particle.
+Each particle corresponds to a different initial condition.
+=#
+
+#=
+ Let's compute the trajectories, $\mathbf{r}_p(t)$, of a few particles $p = 1,\ldots,n$, in
+ this steady velocity field. We will release them from some places upstream of the airfoil and
+ see where they go over a time interval.
+=#
+
+#=
+Initial locations of the particles. Each pair is the initial coordinates of a particle. You can (and should) play
+around with these, add more particles, etc.
+=#
+pts = [ [-1,0.25], [-1,0], [-1,-0.025], [-1,-0.05], [-1,-0.25],[-1,-0.5],  [-1,-0.75] ];
+
+#=
+Time range over which to compute the trajectory (from t = 0 to t = Tmax). You might need to change this to
+make the trajectories go far enough.
+=#
+Tmax = 10.0;
+
+#=
+Compute the trajectories.
+=#
+traj_array = compute_trajectory(vel,sys,pts,Tmax);
+
+# Each entry in the output array `traj_array` contains a trajectory of a particle. For example,
+# `traj_array[1]` contains the trajectory of particle 1, $\mathbf{r}_1(t)$.
+
+# Plot the trajectory, $\mathbf{r}_p(t)$, of each particle
+trajectories(traj_array,sys)
+
+#=
+## Particle acceleration and the material derivative of velocity
+Now, let's develop some intuition for how the particle's velocity changes along its
+trajectory---i.e., the acceleration. From the particle's perspective (the Lagrangian perspective),
+this is simply
+
+$$ \dfrac{\mathrm{d}\mathbf{V}}{\mathrm{d}t} $$
+
+But earlier, we equated the particle's velocity to the local fluid velocity field. If we take the
+time derivative of both sides of this equation, then, by the chain rule, we get
+
+$$ \dfrac{\mathrm{d}\mathbf{V}}{\mathrm{d}t} = \dfrac{\mathrm{d}\mathbf{r}}{\mathrm{d}t} \cdot \nabla \mathbf{u}(\mathbf{r}(t),t) + \dfrac{\partial \mathbf{u}}{\partial t} (\mathbf{r}(t),t).$$
+
+Notice the appearance of the particle velocity ($\mathrm{d}\mathbf{r}/\mathrm{d}t$) on the
+right-hand side. We can replace this with the local fluid velocity. Let's also switch the order
+of the terms, as is conventional:
+
+$$ \dfrac{\mathrm{d}\mathbf{V}}{\mathrm{d}t} = \dfrac{\partial \mathbf{u}}{\partial t}(\mathbf{r}(t),t) + \mathbf{u} \cdot \nabla \mathbf{u}(\mathbf{r}(t),t).$$
+
+The right hand side is called the material derivative of velocity, $D\mathbf{u}/Dt$. It contains
+two terms: a **convective acceleration** ($\mathbf{u}\cdot\nabla\mathbf{u}$), due to spatial changes of velocity along the particle's path, and an **unsteady term** ($\partial\mathbf{u}/\partial t$), due to time changes in the velocity field itself.
+
+Like the fluid velocity, it is a field quantity (a function of space and time, independently of
+each other). Here, it is evaluated at the location of the particle:
+
+$$ \dfrac{\mathrm{d}\mathbf{V}}{\mathrm{d}t} = \dfrac{D \mathbf{u}}{D t} (\mathbf{r}(t),t)$$
+=#
+
+
+#=
+### Material derivative in action
+Let's explore this equality on our steady airfoil flow. In such a flow, the material
+derivative just contains the convective acceleration:
+
+$$ \dfrac{D \mathbf{u}}{D t} = \mathbf{u}\cdot\nabla\mathbf{u}$$
+
+So if we compute the convective acceleration field and then evaluate it along the particle's
+trajectory, this should be equal to the particle's acceleration. Let's try this.
+=#
+
+# #### Velocity along the trajectory
+# First, let us calculate the velocity along one of the particle trajectories:
+particle_number = 2
+traj = traj_array[particle_number];
+
+#=
+Plot the velocity components as a function of the $x$ component of the trajectory.
+This allows us to see how these components change as the particle encounters the airfoil.
+We also plot the particle's trajectory for reference.
+=#
+fieldtrajectory(traj,vel,sys,fieldlabel="Velocity")
+
+#=
+Notice how the particle's u component drops as it encounters the nose of the airfoil, while
+the v component gets larger as the particle moves toward the top of the airfoil.
+
+The u component then **accelerates rapidly**---to a value even larger than the background flow
+speed---as it passes over the upper surface. (By the way: this aspect is essential for generating lift.)
+
+Then both components get smaller as the particle passes over the rear part of the wing.
+=#
+
+# #### Plotting other quantities along the trajectory
+# Let's look at another important quantity along the trajectory: the pressure.
+
+press = ViscousFlow.pressure(u,sys,t)
+fieldtrajectory(traj,press,sys,fieldlabel="Pressure")
+
+#=
+Note that pressure rises dramatically as the particle gets close to the nose of the airfoil.
+It is nearly achieving its stagnation value there. Then, as the particle passes over the upper
+surface, it becomes negative. This is the suction experienced on the upper side of the wing, necessary for
+achieving lift. Eventually, the pressure relaxes back to zero in the wake of the wing.
+=#
+
+#=
+#### Rate of change of particle velocity
+Let's evaluate the rate of change of this particle velocity. We do this by
+taking the time derivative of the velocity along the particle's path. The `fieldtrajectory`
+plotting function enables this with an extra argument `deriv=1`:
+=#
+fieldtrajectory(traj,vel,sys,fieldlabel="Acceleration",deriv=1)
+
+#=
+Now let's compute the convective acceleration field, $\mathbf{u}\cdot\nabla\mathbf{u}$,
+and evaluate it along the particle's trajectory.
+=#
+ugradu = convective_derivative(vel,sys,t);
+
+#=
+Plot the acceleration components. We will compare the particle's acceleration,
+$\mathrm{d}\mathbf{V}/\mathrm{d}t$, to the convective acceleration evaluated along
+the trajectory, $\mathbf{u}\cdot\nabla\mathbf{u}(\mathbf{r}(t))$. They should be equal:
+=#
+fieldtrajectory(traj,ugradu,sys,fieldlabel="Convective accel")
+fieldtrajectory!(traj,vel,sys,fieldlabel="Acceleration",deriv=1)
+#=
+The two sets of components lie right on top of each other. In other words, they are identical!
+
+This confirms that these two ways of viewing the acceleration---the Lagrangian view from the
+particle's perspective, and the Eulerian calculation of an acceleration field (the convective
+derivative)---are, indeed, the same. (If this were an unsteady field, we would need to also
+account for the unsteady part of the acceleration.)
+=#
+
+#=
+## Streamlines and the streamfunction
+Streamfunction, $\psi$, is another important quantity in fluid dynamics, and we will
+return to this later. What is its value along the particle trajectory? Let's find out:
+=#
+ψ = ViscousFlow.streamfunction(u,sys,t);
+
+# Plot the streamfunction as a function of $x$ along trajectory:
+fieldtrajectory(traj,ψ,sys,fieldlabel="Streamfunction")
+
+#=
+It's constant! Why? Because **streamfunction is constant along streamlines**, and in steady flow,
+the **particle pathlines are identical to streamlines**.
+=#
---
+++ literate/1.2-PlottingFlowFields.jl
@@ -0,0 +1,173 @@
+# <img src="logo_UCLA_blue_boxed.svg" alt="ucla logo" width="150" align="left"/>
+
+# ### **MAE 150A - Intermediate Fluid Mechanics**
+# <hr />
+
+#=
+# Plotting a steady flow field
+In this notebook we will discuss ways that we plot a flow field. We will first
+discuss velocity profiles. Then, we will inspect the pre-computed steady flow past
+an airfoil again.
+=#
+
+# ### Set up the module
+using MAE150A
+#-
+using Plots
+
+#=
+### Velocity profiles
+One of the most basic ways that we plot a flow field is by plotting a **velocity profile**.
+This gives us a means of seeing how the flow speed varies with position.
+=#
+
+#=
+As an example, let us consider the most basic of flowfields: the steady Couette flow.
+This is the flow generated between two parallel walls, when one of the walls is in motion
+and the other one is at rest. The velocity field is steady and *unidirectional*: it only moves
+in one direction (the $x$ direction, parallel to the walls). The velocity field has only one
+component, in the $x$ direction, and varies only with distance from the lower wall $y = 0$.
+
+$$u(y) = U y/H$$
+
+where $U$ is the speed of the upper wall and $H$ is the height of the gap between the walls.
+=#
+
+#=
+We plot the velocity profile opposite to how we would usually plot a function: with $u$ on
+the horizontal axis and $y$ on the vertical axis. Also, it is far preferred to plot this
+**nondimensionally**: plot $u/U$ vs $y/H$. This ensures that the plot can be understood more
+universally, rather than specific to just one wall speed and one gap height.
+
+In Julia, one useful idea is to define a function with *keyword arguments* that take default
+values if we omit them. Here, we will use this to define the velocity function so that it can
+behave either dimensionally (by setting $U$ and $H$ explicity) or nondimensionally (by not setting
+them, so that $U$ and $H$ each default to 1). By doing the latter, then '$y$' actually represents
+$y/H$ and '$u$' represents $u/U$.
+=#
+
+u_couette(y;U=1,H=1) = U*y/H
+
+#=
+Now generate a set of points in $y$ (between 0 and 1), evaluate $u$, and plot the profile:
+=#
+y = 0:0.01:1
+plot(u_couette.(y),y,xlims=(0,1),ylims=(0,1),xguide="u/U",yguide="y/H",framestyle=:box,title="Couette flow profile")
+
+#=
+It should be understood that this profile signifies that the flow is going horizontally,
+remains at rest at the bottom $y=0$, and is fastest at the top $y = H$.
+=#
+
+# A channel Poiseuille flow profile would be parabolic:
+u_pois(y;U=1,H=1) = 4*U*y*(H-y)/H^2
+#-
+plot(u_pois.(y),y,xlims=(0,1),ylims=(0,1),xguide="u/U",yguide="y/H",framestyle=:box,title="Poiseuille flow profile")
+
+#=
+### Other ways we visualize a flow
+The velocity profile is only one way to understand a flow, and it only is helpful
+in certain circumstances, such as near a wall. For more complex flows, we need some additional
+tools to visualize a flow.
+=#
+
+#=
+We will use the flow from the same pre-computed CFD solution we used in the previous notebook.
+This is the steady state flow past a NACA 4415 airfoil at 5 degrees angle of attack at Reynolds
+number 500.
+=#
+filename = "NACA4415Re500.jld"
+u, t, sys = load_ns_solution(filename);
+
+#=
+First, let's get the flow fields associated with the flow state: velocity (`vel`),
+vorticity (`ω`), streamfunction (`ψ`), and pressure (`press`)
+=#
+vel = ViscousFlow.velocity(u,sys,t)
+ω = ViscousFlow.vorticity(u,sys,t)
+ψ = ViscousFlow.streamfunction(u,sys,t)
+press = ViscousFlow.pressure(u,sys,t);
+
+# ### Plotting the vorticity field
+plot(ω,sys,levels=range(-15,15,length=30), color = :RdBu,clim=(-15,15),ylim=(-1,1),xlim=(-1,3),
+      size=(900,300),colorbar=:true,title="Vorticity field")
+#=
+This is a contour plot. Each line represents a contour of constant value of the
+quantity we are plotting. Here, that value is **vorticity**.
+
+Vorticity represents the local rate of rotation in the flow. Because of the no-slip
+condition on the surface of the wing, the motion of fluid passing over the wing imparts
+a clockwise rotation to the fluid near the wing. This corresponds to negative vorticity.
+(Rembember the "right-hand rule".) Similarly, fluid motion under the wing imparts
+counter-clockwise rotation, or positive vorticity.
+
+These regions of vorticity are called *boundary layers*. We will discuss them
+extensively later in the term. There are three key things to observe about them here:
+* The boundary layer on the upper surface is actually **separating** from the wing.
+  This is an indication of stall! Stall, as you probably know, is not good for flight.
+* Behind the wing is the **wake**. The wake is actually quite large in this case
+because of the separated flow. In aerodynamics we generally desire attached flow and
+a narrow wake.
+* Outside of the boundary layers, the flow is entirely **irrotational**. There
+is fluid motion out there, but no rotational motion in that flow.
+=#
+
+
+#=
+### Plot the streamfunction field
+In this case, we also will create a line contour plot, but use the same color for each
+contour. Why do we do this? Maybe you have a guess once you look at it.
+=#
+plot(ψ,sys,levels=31, color = :Black, xlim = (-1,3),ylim=(-0.5,0.5),size=(900,400),title="Streamfunction field")
+
+#=
+These lines of constant streamfunction are **streamlines**. You can see the separated
+flow region quite clearly as the region with "missing" streamlines on the upper surface.
+=#
+
+#=
+### Plot the velocity field components
+This is a vector field, so we will plot the components separately.
+=#
+plot(
+    plot(vel.u,sys,size=(900,300),levels=range(-0.1,1.5,length=31), color = :RdBu,clim=(-0.1,1.5),xlim=(-1,3),ylim=(-0.5,0.5),title="x component of velocity"),
+    plot(vel.v,sys,size=(900,300),color=:RdBu,levels=range(-0.15,0.15,length=31),clim=(-0.15,0.15),xlim=(-1,3),ylim=(-0.5,0.5),title="y component of velocity"),
+    layout=(2,1),
+    size=(800,400)
+    )
+#=
+There is not as much important to say about these components. It is not very
+typical to plot individual components like this, and it is challenging to interpret
+these plots.
+=#
+
+#=
+### Plot the magnitude of the velocity field
+This field is the speed of the flow.
+=#
+umag = mag(vel)
+plot(umag,sys,color = :RdBu, ylim = (-0.5,0.5),xlim = (-1,3),size=(900,400),title="Velocity magnitude field",colorbar=:true)
+
+#=
+Notice that the wake is clearly shown in this field, distinctly slower than the rest
+of the flow.
+=#
+
+#=
+### Pressure coefficient field
+Now let's plot the pressure coefficient field. The pressure coefficient $C_p$ is just a
+normalization of the pressure:
+
+$$C_p = \dfrac{p-p_\infty}{\frac{1}{2} \rho U_\infty^2}$$
+
+In this case, the free stream and density are both equal to 1, so we just divide pressure by
+0.5 (i.e., multiply pressure by 2) to get the pressure coefficient.
+=#
+plot(2*press,sys,levels=range(-1.5,1.5,length=31),color=:RdBu,clim=(-1.5,1.5),xlim=(-1,1),ylim=(-0.5,0.5),
+        colorbar=:true,size=(900,400),title="Pressure coefficient")
+#=
+There are two notable features in the pressure. Along the upper part of the wing,
+the pressure is very low. This is the **suction** side of the wing, where it generates
+most of its lift. Also, there is a region of higher pressure at the front of the wing
+where the flow stagnates.
+=#
