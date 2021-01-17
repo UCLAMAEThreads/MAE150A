681c0a98673da91e5379491e7bc63bfaff37cbe0
Jeff Eldredge <jdeldre@g.ucla.edu>
2021-01-16 23:32:08 -0800

    Moved literate source files into test dir

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,21 @@
 using MAE150A
+using ViscousFlow
 using Test
+using Literate
 
-include("trajectories.jl")
+#include("trajectories.jl")
+
+outputdir = "../outputtest"
+for (root, dirs, files) in walkdir("literate")
+    if splitpath(root)[end] == "assets"
+        for file in files
+            cp(joinpath(root, file),joinpath(outputdir,file),force=true)
+        end
+    end
+end
+
+for (root, dirs, files) in walkdir("literate")
+    for file in files
+        endswith(file,".jl") && Literate.notebook(joinpath(root, file),outputdir=outputdir)
+    end
+end
