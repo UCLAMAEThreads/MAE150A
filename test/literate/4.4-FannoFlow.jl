5560110ffc1a19b295fe4980ae4df740d8c43b05
Jeff Eldredge <jdeldre@g.ucla.edu>
2021-03-04 00:13:10 -0800

    Added notebooks 4.4 and 4.5

---
+++ test/literate/4.4-FannoFlow.jl
@@ -0,0 +1,141 @@
+include("header.jl")
+
+#=
+# Steady frictional flow through a constant-area duct
+This notebook demonstrates the use of tools for computing flow through a constant-area
+ duct with frictional effects, also known as **Fanno flow**
+=#
+
+# ### Set up the module
+using MAE150A
+#-
+using Plots
+
+#=
+### Some general aspects of Fanno flow
+Most calculations with frictional flow involve the relationship between Mach
+number and the reference length $L^*$, representing the length required from that
+point to bring the flow to Mach number equal to 1. The relationship is based on
+the dimensionless parameter
+
+$$ \dfrac{f L^*}{D}$$
+
+where $f$ is the Darcy friction factor and $D$ is the duct diameter. Let's plot
+ this relationship for air:
+=#
+Mrange = range(0.1,8,length=801)
+fLDarray = []
+for M in Mrange
+    push!(fLDarray,value(FLStarOverD(MachNumber(M),FannoFlow,gas=Air)))
+end
+#-
+plot(Mrange,fLDarray,xlim=(0,8),ylim=(0,10),xticks=0:1:8,xlabel="Mach number",ylabel=L"fL^*/D",legend=false)
+
+#=
+Notice that the reference length goes to zero at Mach number 1, as it should, by
+its definition. It should also be noted that there are a few values of $fL^*/D$ for
+which there are two possible Mach numbers---one subsonic and one supersonic. This
+is analogous to the situation in isentropic flow with area changes.
+=#
+#=
+### Example 1
+Flow enters a duct of diameter 2 cm at Mach number equal to 0.1 and leaves at
+Mach number equal to 0.5. Assume that the friction factor is equal to 0.024 throughout.
+
+(a) What is the length of the duct?
+
+(b) How much longer would the duct have to be to reach Mach number equal to 1 at the exit?
+=#
+#=
+First, we use $M_1 = 0.1$ to calculate $f L_1^*/D$. We will do the same with $M_2$
+ to calculate $f L_2^*/D$. The actual length $L$ is given by the difference between
+ $L_1^*$ and $L_2^*$.
+=#
+f = FrictionFactor(0.024)
+D = Diameter(2,units=CM)
+#-
+M1 = MachNumber(0.1)
+fL1star_over_D = FLStarOverD(M1,FannoFlow)
+#-
+M2 = MachNumber(0.5)
+fL2star_over_D = FLStarOverD(M2,FannoFlow)
+# Now calculate $L_1^*$ and $L_2^*$, and $L$ from their difference:
+L1star = Length(fL1star_over_D*D/f)
+L2star = Length(fL2star_over_D*D/f)
+L = Length(L1star-L2star)
+# The additional length needed to bring the flow to Mach number 1 is, by definition, $L_2^*$:
+L2star
+# So we only need 0.89 m more to bring the flow to sonic (choked) conditions.
+
+#=
+###  Example 2
+Consider a flow of air through a circular pipe of diameter 3 cm, starting at
+stagnation pressure 200 kPa, stagnation temperature 500 K, and velocity 100 m/s.
+The friction factor in the pipe can be assumed to be 0.02 throughout.
+
+(a) What length of pipe would be needed to drive the flow to Mach number 1 at the exit?
+
+(b) What is the mass flow rate through the pipe at those conditions?
+
+(c) Suppose the pipe were 30 m long. How would the mass flow rate change?
+=#
+
+# For (a), we are seeking $L_1^*$. First set the known conditions:
+D = Diameter(3,units=CM)
+p01 = StagnationPressure(200,units=KPa)
+T01 = StagnationTemperature(500)
+u1 = Velocity(100)
+f = FrictionFactor(0.02)
+
+#=
+Now calculate the Mach number at the entrance. For this, we need $c_1$, the speed
+of sound. We get this by using the known stagnation temperature $T_{01} = 500$ K
+and velocity $u_1 = 100$ m/s. We will calculate the temperature $T_1$ from these
+(using $h_1 = h_{01} - u_1^2/2$), and $c_1$ from that:
+=#
+h01 = StagnationEnthalpy(T01)
+h1 = Enthalpy(h01,u1) # this computes h1 = h01 - u1^2/2
+T1 = Temperature(h1)
+c1 = SoundSpeed(T1)
+M1 = MachNumber(u1/c1)
+# Now we can compute $fL_1^*/D$ and then $L_1^*$. This is what we seek.
+fL1starD = FLStarOverD(M1,FannoFlow)
+L1star = Length(fL1starD*D/f)
+
+#=
+so we need a pipe of 16.6 m to reach Mach number 1.
+
+Now let's compute the mass flow rate. We need the area and density. For the density,
+we will first get the stagnation density (from $p_{01}$ and $T_{01}$) and then get
+ density from the isentropic relation. (We are not using isentropic relations
+ between two different points in the duct; we are using it to relate *local*
+ stagnation conditions to *local* conditions.)
+=#
+A = Area(D)
+ρ01 = StagnationDensity(p01,T01)
+ρ1 = Density(ρ01,M1,Isentropic)
+# and the mass flow rate is:
+ṁ = MassFlowRate(ρ1*u1*A)
+
+#=
+(c) Now suppose the pipe is 30 m long. This becomes our new $L_1^*$, and all of
+the conditions at the entrance must adjust accordingly. First find the Mach number
+=#
+Lstar = L = Length(30)
+fLstar_over_D = FLOverD(f*Lstar/D)
+M1 = MachNumber(fLstar_over_D,FannoFlow)
+
+#=
+Note that the entrance Mach number is lower. The flow slows down due to the longer pipe.
+Now find the new values of $\rho_1$ and $u_1$ (by finding $c_1$):
+=#
+T1 = Temperature(T01,M1,Isentropic)
+ρ1 = Density(ρ01,M1,Isentropic)
+c1 = SoundSpeed(T1)
+u1 = Velocity(c1,M1)
+ṁ = MassFlowRate(ρ1*u1*A)
+
+#=
+So the mass flow rate is smaller than it was with the shorter pipe! For the same
+reservoir conditions, less mass flow rate is delivered to a longer (choked) pipe.
+=#
---
+++ test/literate/4.5-RayleighFlow.jl
@@ -0,0 +1,164 @@
+include("header.jl")
+
+#=
+# Steady flow through a duct with heat transfer
+This notebook demonstrates the use of tools for computing steady compressible flow
+through a constant-area duct with heat transfer, commonly known as **Rayleigh flow**.
+=#
+
+# ### Set up the module
+using MAE150A
+#-
+using Plots
+
+#=
+# Steady flow through a duct with heat transfer
+This notebook demonstrates the use of tools for computing steady compressible flow
+through a constant-area duct with heat transfer, commonly known as **Rayleigh flow**.
+=#
+#=
+#### Simple example
+In a duct with helium, if the flow enters with stagnation temperature of 20 degrees C,
+how much does the stagnation temperature change if we add heat 400 kJ/kg?
+=#
+q = HeatFlux(400,units=KJPerKG)
+T01 = StagnationTemperature(20,units=C)
+
+# starting stagnation enthalpy
+h01 = StagnationEnthalpy(T01,gas=He) # h01 = cp*T01
+
+# add the heat
+h02 = StagnationEnthalpy(h01 + q)
+
+# calculate the final stagnation temperature
+T02 = StagnationTemperature(h02,gas=He)  # T02 = h02/cp
+
+# Report the final value in Celsius:
+value(T02,C)
+# so the flow exiting the duct has stagnation temperature 97 C.
+
+#=
+The sonic state is used as a reference, similar to Fanno flow and isentropic flow.
+All states share the same $T_{0}^*$, $p^*$, $u^*$, etc, so we can relate two points
+via this reference. We have functions that allow us to do this. For example, to
+find the ratio of the local stagnation temperature to its sonic reference value
+at Mach number 0.5 in air:
+=#
+T0OverT0Star(MachNumber(0.5),RayleighFlow,gas=Air)
+
+#=
+Note that the argument `RayleighFlow` was used to designate this function's use.
+If the Mach number is 1, then...
+=#
+T0OverT0Star(MachNumber(1),RayleighFlow,gas=Air)
+
+#=
+That is, $T_{0}$ is equal to $T_{0}^*$ when Mach number is 1, as it should be,
+by definition.
+=#
+#=
+Alternatively, if we know $T_0/T_0^*$, we can determine $M$. However, there are
+sometimes two possible values of Mach number, as with Fanno flow and isentropic flow.
+=#
+Mrange = range(0.001,8,length=801)
+T0_over_T0star = []
+for M in Mrange
+    push!(T0_over_T0star,value(T0OverT0Star(MachNumber(M),RayleighFlow,gas=Air)))
+end
+#-
+plot(Mrange,T0_over_T0star,xlim=(0,8),ylim=(0,2),xticks=0:1:8,xlabel="Mach number",ylabel=L"T_0/T_0^*",legend=false)
+plot!(Mrange,0.75*ones(length(Mrange)),style=:dash)
+
+#=
+For example, if $T_0/T_0^* = 0.75$, there are two possible Mach numbers, one subsonic
+and one supersonic. The correct one depends on the circumstances.
+=#
+Msub, Msup = MachNumber(TemperatureRatio(0.75),RayleighFlow,gas=Air)
+#-
+Msub
+#-
+Msup
+
+#=
+### Example
+Suppose the flow of air in a duct enters with velocity 75 m/s at a pressure of
+150 kPa and temperature 300 K. We add heat 900 kJ/kg. Find
+
+(a) The value of the Mach number at the entrance and exit
+
+(b) The velocity, pressure, and temperature at the exit
+
+(c) The maximum amount of heat we can add so that the flow is just choked
+=#
+# Set the given values
+u1 = Velocity(75)
+p1 = Pressure(150,units=KPa)
+T1 = Temperature(300)
+q = HeatFlux(900,units=KJPerKG)
+
+# We can immediately find the Mach number at the location 1, by finding $c_1$:
+c1 = SoundSpeed(T1,gas=Air)
+M1 = MachNumber(u1/c1)
+
+# Now, use $M_1$ and $T_1$ to find $T_{01}$.
+T01 = StagnationTemperature(T1,M1,Isentropic,gas=Air)
+
+#=
+Now let's focus on state 2. We will use the basic relation $h_{02} = h_{01} + q$
+to determine $T_{02}$.
+=#
+h01 = StagnationEnthalpy(T01,gas=Air)
+h02 = StagnationEnthalpy(h01+q)
+T02 = StagnationTemperature(h02,gas=Air)
+
+#=
+To get the other states at location 2, we need the sonic reference conditions.
+For example, we will find
+
+$$T_{02}/T_{0}^*$$
+to determine $M_2$. We get this by calculating
+
+$$T_{0}^* = T_{01}/(T_{01}/T_{0}^*)$$
+=#
+T0star = StagnationTemperature(T01/T0OverT0Star(M1,RayleighFlow,gas=Air))
+#-
+T02_over_T0star = TemperatureRatio(T02/T0star)
+#-
+M2sub, M2sup = MachNumber(T02_over_T0star,RayleighFlow,gas=Air)
+#=
+The subsonic one is the only possible candidate, because the flow came in at
+subsonic speed and can't have passed through Mach number 1. Thus
+=#
+M2 = M2sub
+# We have increased the Mach number from 0.216 to 0.573 by adding heat.
+
+# Now find the other reference states that we will use:
+pstar = Pressure(p1/POverPStar(M1,RayleighFlow,gas=Air))
+ustar = Velocity(u1/VOverVStar(M1,RayleighFlow,gas=Air))
+Tstar = Temperature(T1/TOverTStar(M1,RayleighFlow,gas=Air))
+
+#=
+and now the values themselves. For example, we use $M_2$ to calculate $p_2/p^*$
+and then calculate $p_2 = p^*(p_2/p^*)$.
+=#
+p2 = Pressure(pstar*POverPStar(M2,RayleighFlow,gas=Air))
+#-
+u2 = Velocity(ustar*VOverVStar(M2,RayleighFlow,gas=Air))
+#-
+T2 = Temperature(Tstar*TOverTStar(M2,RayleighFlow,gas=Air))
+#=
+Finally, let us calculate the maximum heat flux -- the heat flux that brings the
+flow just to sonic conditions when starting at the given conditions at location 1.
+This comes from
+
+$$ q_{max} = h_{0}^* - h_{01} = c_p (T_0^* - T_{01})$$
+
+We have a convenience function for that, based on stagnation temperature:
+=#
+qmax = HeatFlux(T01,T0star,gas=Air)
+#-
+value(qmax,KJPerKG)
+#=
+Thus, we can add up to 1223 kJ/kg before the flow gets choked. Any more than that
+will cause the entrance conditions to change.
+=#
