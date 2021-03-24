198b2d6521210a6724ee656ba3d689846078a1a4
Jeff Eldredge <jdeldre@g.ucla.edu>
2021-03-24 11:54:28 -0700

    Fixed URL in header

--- test/literate/header.jl
+++ test/literate/header.jl
@@ -1,4 +1,4 @@
-# <img src="https://raw.githubusercontent.com/jdeldre/MAE150A/master/notebook/logo_UCLA_blue_boxed.svg" alt="ucla logo" width="150" align="left"/>
+# <img src="https://raw.githubusercontent.com/UCLAMAEThreads/MAE150A/master/notebook/logo_UCLA_blue_boxed.svg" alt="ucla logo" width="150" align="left"/>
 
 # ### **MAE 150A - Intermediate Fluid Mechanics**
 # <hr />
